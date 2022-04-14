9f90bcbf0b91e57b8ffb9152a88672c444b6f5f5
ButlerMW <butler.m.w@outlook.com>
2022-04-14 13:33:00 -0600

    Updateing BTree

--- src/main/java/cs321/btree/TreeObject.java
+++ src/main/java/cs321/btree/TreeObject.java
@@ -1,6 +1,27 @@
 package cs321.btree;
 
-public class TreeObject<E>
+public class TreeObject
 {
+    private long key;
+    private int frequency = 0;
 
+    public TreeObject(long key)
+    {
+        this.key = key;
+    }
+
+    public void incrementFrequency()
+    {
+        frequency++;
+    }
+
+    public long getKey()
+    {
+        return key;
+    }
+
+    public int getFrequency()
+    {
+        return frequency;
+    }
 }
