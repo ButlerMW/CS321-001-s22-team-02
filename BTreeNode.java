73ac84841b7857168d9b57a4dd4858ac04629610
Abdullah Faizi <abdullahmohammad530@u.boisestate.edu>
2022-04-13 19:26:50 -0600

    added more

--- BTreeNode.java
+++ BTreeNode.java
@@ -1,9 +1,16 @@
 import java.util.LinkedList;
 
 public class BTreeNode {
+
     private long parent;
     private boolean leaf;
     private int numberOfKeys;
     private LinkedList<Long> children;
+    private LinkedList<TreeObject> k;
+
+    public BTreeNode() 
+    {
+        numberOfKeys = 0;
+    }
 
 }
